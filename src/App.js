463a39ef0aee16cf257b55e5649632d6fff60601
YUANJA3 <yuanja3@oocl.com>
2025-09-12 14:54:57 +0800

    feat: implement useReducer practise.

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import { TodoContext } from "./contexts/TodoContext";
 function App() {
   // the Hooks API manage component data state
   const [state, dispatch] = useReducer(todoReducer, initialState);
-
+  const value={state, dispatch};
   return (
     <div className="App">
-      <TodoContext.Provider value={{state, dispatch}}>
+      <TodoContext.Provider value={value}>
         <TodoList/>
       </TodoContext.Provider>
     </div>
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,21 @@
+import {useContext} from "react";
+import {TodoContext} from "../contexts/TodoContext";
+
 const TodoList = () => {
+  const {state, dispatch} = useContext(TodoContext);
   return (
-      <div>This is the TodoList Component.</div>
-  );
+      <div>
+  <div>This is the TodoList Component.</div>
+  {
+    state.map((todo) => {
+       return <div >
+          {todo.text}
+        </div>
+})
+  }
+      </div>
+)
+  ;
 }
 
 export default TodoList
