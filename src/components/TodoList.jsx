132ad08ac8705b4e6a9668675bf2f3bdb002f1bd
YUANJA3 <yuanja3@oocl.com>
2025-09-12 15:03:33 +0800

    feat: change todo-items style

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,14 +1,15 @@
 import {useContext} from "react";
 import {TodoContext} from "../contexts/TodoContext";
-
+import '../TodoList.css';
 const TodoList = () => {
   const {state, dispatch} = useContext(TodoContext);
   return (
-      <div>
+      <div className={"todo-group"}>
   <div>This is the TodoList Component.</div>
   {
     state.map((todo) => {
-       return <div >
+       return <div className={"todo-item"} >
+           <input type={"checkbox"} value={false}/>
           {todo.text}
         </div>
 })
